56cdbed6128073176e87298e9f992691b6c27ff3
slikts <dabas@untu.ms>
2017-05-13 13:41:37 +0300

    Test stub

---
+++ test/test.ts
@@ -0,0 +1,14 @@
+import Blueprint from '../src/Blueprint';
+import * as fs from 'fs'
+
+import * as test from 'tape'
+
+test('basic import', (t) => {
+  t.plan(1)
+  const bps = fs.readFileSync('test/balancer.bp', 'utf8')
+
+  const bp = new Blueprint(bps)
+
+  const json = JSON.stringify(bp.toObject())
+  t.equals(json, JSON.stringify(JSON.parse(fs.readFileSync('test/balancer.json', 'utf8'))))
+})
